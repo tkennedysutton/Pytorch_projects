3b9479007f37226003bc0cb1d4405d97fec29574
tkennedysutton <tristan.c.kennedy@gmail.com>
2025-04-09 10:47:37 +0100

    Re-add important files after rm --cached

---
+++ Day1_notes.py
@@ -0,0 +1,7 @@
+# Day 1 training, firstly importing torch to play with it
+import torch
+# create a 1 dimensional tensor with values [1,2,3]
+tensor a=torch.tensor([1,2,3])
+# print the tensor 
+print("Tensor A:", tensor_a)
+
